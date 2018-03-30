2c90e051e91f361f62403b4f59eb133027998990
anoff <offenhaeuser@gmail.com>
2018-03-30 11:18:31 +0200

    :star2: feat: add _some_ tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const {getForecast} = require('./lib/weather')
+const {getForecast, mapPrettifyResults} = require('./lib/weather')
 
 module.exports = (req, res) => {
   getForecast('Stuttgart,DE')
+    .then(result => result.map(mapPrettifyResults))
     .then(weather => res.end(JSON.stringify(weather, null, 2)))
 }
